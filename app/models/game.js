d62949f97cbaabf755558c8ff77e8a8ffe4931e1
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.com>
2012-12-20 18:20:45 +0400

    Move to compound

--- app/models/game.js
+++ app/models/game.js
@@ -1,51 +1,54 @@
-var reversi = require(app.root + '/public/javascripts/reversi.js');
+module.exports = function (compound) {
+    var app = compound.app;
+    var reversi = require(app.root + '/public/javascripts/reversi.js');
 
-function Game() {
-    this.reversi = reversi.createGame();
-}
-
-Game.prototype.join = function (id) {
-    if (!this.starter) {
-        console.log('Starter joined', id);
-        this.starter = id;
-    } else {
-        console.log('Opponent joined', id);
-        this.opponent = id;
+    function Game() {
+        this.reversi = reversi.createGame();
     }
-    return this.reversi.join();
+
+    Game.prototype.join = function (id) {
+        if (!this.starter) {
+            console.log('Starter joined', id);
+            this.starter = id;
+        } else {
+            console.log('Opponent joined', id);
+            this.opponent = id;
+        }
+        return this.reversi.join();
+    };
+
+    Game.prototype.state = function (player) {
+        if (!this.opponent) {
+            return 'wait opponent';
+        } else if (this.reversi.can_player_move(player)) {
+            return 'move';
+        } else {
+            return 'wait';
+        }
+    };
+
+    Game.prototype.move = function (player, coords) {
+        this.player = player;
+        return this.reversi.move(coords);
+    };
+
+    Game.prototype.other = function (id) {
+        return this.opponent === id ? this.starter : this.opponent;
+    };
+
+    Game.prototype.end = function () {
+        return this.reversi.board.terminal_board;
+    };
+
+    Game.prototype.winner = function () {
+        var s = this.reversi.board.board_stats;
+        return s.b > s.w ? 'b' : 'w';
+    };
+
+    Game.prototype.boardToJSON = function () {
+        return JSON.stringify(this.reversi.board.position);
+    };
+
+    compound.models.Game = Game;
+
 };
-
-Game.prototype.state = function (player) {
-    if (!this.opponent) {
-        return 'wait opponent';
-    } else if (this.reversi.can_player_move(player)) {
-        return 'move';
-    } else {
-        return 'wait';
-    }
-};
-
-Game.prototype.move = function (player, coords) {
-    this.player = player;
-    return this.reversi.move(coords);
-};
-
-Game.prototype.other = function (id) {
-    return this.opponent === id ? this.starter : this.opponent;
-};
-
-Game.prototype.end = function () {
-    return this.reversi.board.terminal_board;
-};
-
-Game.prototype.winner = function () {
-    var s = this.reversi.board.board_stats;
-    return s.b > s.w ? 'b' : 'w';
-};
-
-Game.prototype.boardToJSON = function () {
-    return JSON.stringify(this.reversi.board.position);
-};
-
-module.exports = Game;
-
--- config/environment.js
+++ config/environment.js
@@ -1,17 +1,21 @@
-var express = require('express');
+module.exports = function (compound) {
 
-app.configure(function(){
-    var cwd = process.cwd();
-    
-    app.use(express.static(cwd + '/public', {maxAge: 86400000}));
-    app.set('view engine', 'ejs');
-    app.set('view options', {complexNames: true});
-    app.set('jsDirectory', '/javascripts/');
-    app.set('cssDirectory', '/stylesheets/');
-    app.use(express.bodyParser());
-    app.use(express.cookieParser('secret'));
-    app.use(express.session({secret: 'secret'}));
-    app.use(express.methodOverride());
-    app.use(app.router);
-});
+    var express = require('express');
+    var app = compound.app;
 
+    app.configure(function(){
+        app.use(compound.assetsCompiler.init());
+        app.use(express.static(app.root + '/public', {maxAge: 86400000}));
+        app.set('view engine', 'ejs');
+        app.set('view options', {complexNames: true});
+        app.set('jsDirectory', '/javascripts/');
+        app.set('cssDirectory', '/stylesheets/');
+        app.set('cssEngine', 'stylus');
+        app.use(express.bodyParser());
+        app.use(express.cookieParser('secret'));
+        app.use(express.session({secret: 'secret'}));
+        app.use(express.methodOverride());
+        app.use(app.router);
+    });
+
+};
